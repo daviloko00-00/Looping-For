4df26781fe55e9fad246419ccc3f3abb0018d83d
unknown <davicustodiosilva36@gmail.com>
2025-04-14 14:46:53 -0300

    Código comentado

--- loopingfor.js
+++ loopingfor.js
@@ -3,20 +3,19 @@
 //solicita que o usuário digite um número inteiro maior que 1
 const numero = parseInt(prompt("Digite um número:"));
 
-// inicializa a variável soma com 0, pois não somamos nada ainda
+// inicializa a variável soma com 0, pois não contamos nada ainda
 contagem = 0;
 if (isNaN(numero) || numero <= 1) {
     alert("Erro:\nPor favor, digite um número válido maior que 1.");
 } else {
-    // loop que soma os números de 1 até o número digitado
-
+    // loop que conta os números de 1 até o número digitado
+    let frase = "";
     for (let i = 1; i <= numero; i++){
         contagem = contagem + i;
         // mostra a soma parcial a cada iteração
-        alert(`número ${i}: ${soma}`);
+        frase += `${i} `; // transforma o número em string e adiciona ao resultado
     }
-    
-    alert(`A soma dos números de 1 até ${numero} é: ${soma}`);
+    alert(frase);// mostra a contagem de 1 até o número digitado
 }
         
 
