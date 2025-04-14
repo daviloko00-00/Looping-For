21326852ab0ff9a158f6ef7037686ce3c2fd4a2d
Silas Silva <davicustodiosilva36@gmail.com>
2025-04-14 19:56:41 -0300

    Código finalizado e comentado.

--- loopingfor.js
+++ loopingfor.js
@@ -1,8 +1,23 @@
-const numero1 = parseInt(prompt("Digite o primeiro número:"));
-const numero2 = parseInt(prompt("Digite o segundo número:"));
+const n1 = parseInt(prompt("Digite o primeiro número:"));
+const n2 = parseInt(prompt("Digite o segundo número:"));
 
-if (isNaN(numero1) || isNaN(numero2)) {
+if (isNaN(n1) || isNaN(n2)) {
     alert("Por favor, digite números válidos.");
-} else {let numerosPares = ""; // Variável para armazenar os números pares
-    
+} else {let NumPares = ""; // Variável para armazenar os números pares
+    let inicio = n1 < n2 ? n1 : n2;// o ponto de interrogação é o operador ternário, que é uma forma de simplificar um if-else
+    //Se n1 for menor que n2, o valor de inicio será n1. Se não, será n2.
+    let fim = n1 > n2 ? n1 : n2;
+    for (let i = inicio; i <= fim; i++) {
+        if (i % 2 === 0) {
+            NumPares += i + " ";
+        }
     }
+
+    if (NumPares === "") {
+        alert("Não há números pares no intervalo.");
+    } else {
+        alert("Números pares no intervalo: " + NumPares);
+    }
+}
+    
+    
