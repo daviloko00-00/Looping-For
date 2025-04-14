a4620e653dc18972bdc4452f46198f7cdee48de8
unknown <davicustodiosilva36@gmail.com>
2025-04-14 15:43:50 -0300

    Commit inicial, comentado

--- loopingfor.js
+++ loopingfor.js
@@ -1,15 +1,13 @@
-let soma = 0;// Variável para armazenar os números
-// Loop para solicitar 10 números ao usuário
-for (let i = 1; i <= 10; i++){
-const número = parseInt(prompt(`digite o número ${i}`))
+const número = parseInt(prompt(`digite o número ${i}`));
 if (isNaN(número) || número < 0) {
-alert("Por favor, digite um número válido.");// Verifica se o número é válido
+    alert("Por favor, digite um número válido."); // Verifica se o número é válido
 } else {
-    soma += número;// Adiciona o número à soma
-
+    let tabuada = ""; // Variável para armazenar os números
+    // Loop para solicitar 10 números ao usuário
+    for (let i = 1; i <= 10; i++) {
+        const resultado = número * i; // Calcula o resultado
+        tabuada += `${número} x ${i} = ${resultado}\n`; // Concatena o resultado na string
+    }
+    alert(`Tabuada do ${número}:\n${tabuada}`);
 }
-alert(`Os números digitados foram: ${soma}`);// Exibe os números digitados
-
-}
-
 
