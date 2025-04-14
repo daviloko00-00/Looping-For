4f1ca1fe60d29081d75f5bc5b3a263021645005e
unknown <davicustodiosilva36@gmail.com>
2025-04-14 16:19:33 -0300

    Primeiro commit, comentado, arrumado

--- loopingfor.js
+++ loopingfor.js
@@ -4,4 +4,5 @@ const numero2 = parseInt(prompt("Digite o segundo número:"));
 if (isNaN(numero1) || isNaN(numero2)) {
     alert("Por favor, digite números válidos.");
 } else {let numerosPares = ""; // Variável para armazenar os números pares
+    
     }
