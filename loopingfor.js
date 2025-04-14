98189f30d606a86d618960b3bfa170bdf2c7fa4b
Silas Silva <davicustodiosilva36@gmail.com>
2025-04-14 19:58:11 -0300

    Código finalizado e comentado.

--- loopingfor.js
+++ loopingfor.js
@@ -6,17 +6,18 @@ if (isNaN(n1) || isNaN(n2)) {
 } else {let NumPares = ""; // Variável para armazenar os números pares
     let inicio = n1 < n2 ? n1 : n2;// o ponto de interrogação é o operador ternário, que é uma forma de simplificar um if-else
     //Se n1 for menor que n2, o valor de inicio será n1. Se não, será n2.
-    let fim = n1 > n2 ? n1 : n2;
-    for (let i = inicio; i <= fim; i++) {
+    let fim = n1 > n2 ? n1 : n2;// se n1 for maior que n2, o valor de fim será n1. Se não, será n2.
+    for (let i = inicio; i <= fim; i++) {// o loop for percorre todos os números entre inicio e fim.
+        // O loop começa em inicio e vai até fim, incrementando i em 1 a cada iteração.
         if (i % 2 === 0) {
             NumPares += i + " ";
         }
     }
 
-    if (NumPares === "") {
+    if (NumPares === "") {// no caso de não haver números pares no intervalo, a variável NumPares estará vazia.
         alert("Não há números pares no intervalo.");
     } else {
-        alert("Números pares no intervalo: " + NumPares);
+        alert("Números pares no intervalo: " + NumPares);// se houver números pares, eles serão exibidos em uma caixa de alerta.
     }
 }
     
