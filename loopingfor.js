660060401194a226a27ba477fc1a012b8e388805
unknown <davicustodiosilva36@gmail.com>
2025-04-14 15:49:16 -0300

    primeiro

--- loopingfor.js
+++ loopingfor.js
@@ -1,14 +1,15 @@
 let soma = 0;// Variável para armazenar os números
 // Loop para solicitar 10 números ao usuário
-for (let i = 1; i <= 10; i++){
+for (let i = 1; i <= 5; i++){
 const número = parseInt(prompt(`digite o número ${i}`))
 if (isNaN(número) || número < 0) {
 alert("Por favor, digite um número válido.");// Verifica se o número é válido
 } else {
     soma += número;// Adiciona o número à soma
+    
 
 }
-alert(`Os números digitados foram: ${soma}`);// Exibe os números digitados
+alert(`a soma dos números digitados foram: ${soma}`);// Exibe os números digitados
 
 }
 
