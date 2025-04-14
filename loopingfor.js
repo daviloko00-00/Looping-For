6217883b85cd1e2d965683214423f9403baf7b0b
unknown <davicustodiosilva36@gmail.com>
2025-04-14 15:56:03 -0300

    Primeiro commit, comentado

--- loopingfor.js
+++ loopingfor.js
@@ -4,7 +4,7 @@ for (let i = 1; i <= 5; i++){
 const número = parseInt(prompt(`digite o número ${i}`))
 if (isNaN(número) || número < 0) {
 alert("Por favor, digite um número válido.");// Verifica se o número é válido
-} else {
+} else if (i === 5) {
     soma += número;// Adiciona o número à soma
     media = soma /5;// Calcula a média
     alert(`a média dos números digitados foram: ${media}`);// Exibe a média
