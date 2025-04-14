17522a5edc09ab901f7de1ab6ee4d5abd00019ce
unknown <davicustodiosilva36@gmail.com>
2025-04-14 16:12:38 -0300

    Primeiro commit, comentado, arrumado

--- loopingfor.js
+++ loopingfor.js
@@ -1,21 +1,7 @@
-let soma = 0;// Variável para armazenar os números
-// Loop para solicitar 10 números ao usuário
-for (let i = 1; i <= 5; i++){
-const número = parseInt(prompt(`digite o número ${i}`))
-if (isNaN(número) || número < 0) {
-alert("Por favor, digite um número válido.");// Verifica se o número é válido
-} else {
-    soma += número;// Adiciona o número à soma
-    
-    
+const numero1 = parseInt(prompt("Digite o primeiro número:"));
+const numero2 = parseInt(prompt("Digite o segundo número:"));
 
-} 
-}
-media = soma /5;// Calcula a média
-alert(`a média dos números digitados foram: ${media}`);// Exibe a média
-alert(`a soma dos números digitados foram: ${soma}`);// Exibe os números digitados
-
-
-
-
-
+if (isNaN(numero1) || isNaN(numero2)) {
+    alert("Por favor, digite números válidos.");
+} else {let numerosPares = ""; // Variável para armazenar os números pares
+    }
