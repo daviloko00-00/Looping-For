344900a0c756e7052d5319397f8ed51cb2f90453
unknown <davicustodiosilva36@gmail.com>
2025-04-14 15:29:56 -0300

    Código arrumado, comentado

--- loopingfor.js
+++ loopingfor.js
@@ -1,8 +1,8 @@
 const numeros = [];// Array para armazenar os números
 // Loop para solicitar 10 números ao usuário
 for (let i = 1; i <= 10; i++){
-const meupaunasuabunda = parseInt(prompt(`digite o número ${i}`))
-if (isNaN(meupaunasuabunda) || meupaunasuabunda < 0) {
+const num = parseInt(prompt(`digite o número ${i}`))
+if (isNaN(num) || num < 0) {
 alert("Por favor, digite um número válido.");// Verifica se o número é válido
 } else {
     numeros.push(numeros);// Adiciona o número ao array
