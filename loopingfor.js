60d2e3012d2dec65ee964fbf763ac9d0bf014373
unknown <davicustodiosilva36@gmail.com>
2025-04-28 14:32:35 -0300

    código com identação arrumada com ALT+F+SHIFT

--- loopingfor.js
+++ loopingfor.js
@@ -1,14 +1,14 @@
 let soma = 0;// Variável para armazenar os números
 // Loop para solicitar 10 números ao usuário
-for (let i = 1; i <= 10; i++){
-const número = parseInt(prompt(`digite o número ${i}`))
-if (isNaN(número) || número < 0) {
-alert("Por favor, digite um número válido.");// Verifica se o número é válido
-} else {
-    soma += número;// Adiciona o número à soma
+for (let i = 1; i <= 10; i++) {
+    const número = parseInt(prompt(`digite o número ${i}`))
+    if (isNaN(número) || número < 0) {
+        alert("Por favor, digite um número válido.");// Verifica se o número é válido
+    } else {
+        soma += número;// Adiciona o número à soma
 
-}
-alert(`Os números digitados foram: ${soma}`);// Exibe os números digitados
+    }
+    alert(`Os números digitados foram: ${soma}`);// Exibe os números digitados
 
 }
 
